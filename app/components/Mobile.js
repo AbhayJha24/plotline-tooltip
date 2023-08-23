6bf3e61d9d7423a470044c823e42dce88f31c426
Abhay <abhay.jha243@gmail.com>
2023-08-23 12:32:29 +0530

    Implemented the design of right mobile viewer alongwith responsiveness

--- app/components/Mobile.js
+++ app/components/Mobile.js
@@ -1,5 +1,19 @@
+import styles from "../css/mobile.module.css";
+
 export default function Mobile() {
     return (
-        <main></main>
+        <main className={styles.container}>
+            <section className={styles.screen}>
+                <div className={styles.buttonContainer}>
+                <button className={styles.buttons}>Button 1</button>
+                <button className={styles.buttons}>Button 2</button>
+                </div>
+                <button className={styles.buttons}>Button 3</button>
+                <div className={styles.buttonContainer}>
+                <button className={styles.buttons}>Button 4</button>
+                <button className={styles.buttons}>Button 5</button>
+                </div>
+            </section>
+        </main>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import styles from "./css/main.module.css";
-import SettingForm from "./components/SettingForm";
+import SettingForm from "./components/settingform";
+import Mobile from "./components/mobile";
 
 export default () => {
     return (
         <main className={styles.main}>
             <SettingForm />
+            <Mobile />
         </main>
     );
 }
