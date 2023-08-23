c921694b2fa61e8ad30eb7d579c02e84415cf221
Abhay <abhay.jha243@gmail.com>
2023-08-23 16:03:31 +0530

    Tooltip design implemented by hardcoding as of now

--- app/components/Mobile.js
+++ app/components/Mobile.js
@@ -1,9 +1,15 @@
 import styles from "../css/mobile.module.css";
+import Tooltip from "../components/tooltip";
 
 export default function Mobile() {
     return (
         <main className={styles.container}>
             <section className={styles.screen}>
+
+                {/* Tooltip dynamic component */}
+
+                <Tooltip />
+
                 <div className={styles.buttonContainer}>
                 <button className={styles.buttons}>Button 1</button>
                 <button className={styles.buttons}>Button 2</button>
---
+++ app/components/tooltip.js
@@ -0,0 +1,9 @@
+import style from "../css/tooltip.module.css";
+
+export default function Tooltip() {
+    return (
+        <main className={style.tooltip}>
+            Tooltip text goes here
+        </main>
+    );
+}
